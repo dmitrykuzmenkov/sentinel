25717a83e67b3efabd69a50180ada1f20b643cf2
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-07-04 02:07:20 +0700

    PID_FILE of sentinel now is full path to it

--- env.bash
+++ env.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
-PID_FILE="$0.pid" # Saved in $WORK_DIR
 CHECK_TIMEOUT=3
 TASKS_DIR='./tasks'
 WORK_DIR='./proc'
+PID_FILE=$WORK_DIR/"sentinel.pid"
+
 RELOAD_FILE=$WORK_DIR'/reload' # RELOAD config signal
 QUIT_FILE=$WORK_DIR'/quit' # QUIT signal
 PIDS_FILE=$WORK_DIR'/pids' # All running tasks pids
