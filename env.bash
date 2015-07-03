61e03abd0eaa489b516fe41c5ec78c8a2e3fdc64
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-07-04 02:02:01 +0700

    Move config dir to tasks

--- env.bash
+++ env.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 PID_FILE="$0.pid" # Saved in $WORK_DIR
 CHECK_TIMEOUT=3
-CONFIG_DIR='./config'
+TASKS_DIR='./tasks'
 WORK_DIR='./proc'
 RELOAD_FILE=$WORK_DIR'/reload' # RELOAD config signal
 QUIT_FILE=$WORK_DIR'/quit' # QUIT signal
