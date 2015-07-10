a55dd3d8dd9822c200d287cd45c4da48ddc49e3b
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-07-11 01:09:55 +0700

    Refactor to_bytes function

--- src/helper.bash
+++ src/helper.bash
@@ -2,16 +2,19 @@
 # Convert string 1024M to bytes
 to_bytes() {
   memory=$(echo $1 | grep -oE [0-9]+ || echo 0)
-  if echo $1 | grep -i G &> /dev/null; then
-    memory=$(( $memory * 1024 * 1024 * 1024 ))
-  fi
-
-  if echo $1 | grep -i M &> /dev/null; then
-    memory=$(( $memory * 1024 * 1024 ))
-  fi
-
-  if echo $1 | grep -i K &> /dev/null; then
-    memory=$(( $memory * 1024 ))
-  fi
+  case ${1:0,-1} in
+    t|T)
+      memory=$(( $memory * 1024 * 1024 * 1024 * 1024 ))
+      ;;
+    g|G)
+      memory=$(( $memory * 1024 * 1024 * 1024 ))
+      ;;
+    m|M)
+      memory=$(( $memory * 1024 * 1024 ))
+      ;;
+    k|K)
+      memory=$(( $memory * 1024 ))
+      ;;
+  esac
   echo $memory
 }
