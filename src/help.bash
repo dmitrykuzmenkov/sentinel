226b33a9013f00ba0bdb153915ff7228c6a1541b
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-07-12 22:09:22 +0700

    Help fixes

--- src/help.bash
+++ src/help.bash
@@ -5,12 +5,12 @@ Copyright (c) 2015 by Dmitry Kuzmenkov
 
 Usage: sentinel [options]
 
-  --check-timeout          Timeout in seconds between checks, default: 3
+  --check-timeout          Timeout in seconds between checks. Default: 3
 
-  --tasks-dir              Folder for tasks config files, default: ./tasks
-  --work-dir               Work dir to store log, status, temp files, default: ./proc
-  --status-file            Save status snapshot to file on each check, default: None
-  --pid-file               Sentinel process pid file, default: $WORK_DIR/sentinel.pid
+  --tasks-dir              Folder for tasks config files. Default: ./tasks
+  --work-dir               Work dir to store log, status, temp files. Default: ./proc
+  --status-file            Save status snapshot to file on each check. Default: None
+  --pid-file               Sentinel process pid file. Default: $WORK_DIR/sentinel.pid
 
   --daemonize        -d    Daemonize Sentinel instead of foregroud. Default: not set
   --log-file               Log file for daemon mode. Default: $WORK_DIR/sentinel.log
