91faae807860d9df58b5fe204f325ebff3f9b3be
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-11-02 15:08:26 +0700

    Use signals instead of files: USR1 for reload, HUP for restart, TERM, INT for stop

--- src/env.bash
+++ src/env.bash
@@ -5,9 +5,6 @@ export WORK_DIR='./proc'
 export PID_FILE=$WORK_DIR'/sentinel.pid'
 export LOG_FILE=$WORK_DIR'/sentinel.log'
 
-export RELOAD_FILE=$WORK_DIR'/reload' # RELOAD config signal
-export QUIT_FILE=$WORK_DIR'/quit' # QUIT signal
-export RESTART_FILE=$WORK_FILE'/restart' # RESTAR signal
 export PIDS_FILE=$WORK_DIR'/pids' # All running tasks pids
 
 export EDITOR=$(which nano 2> /dev/null || which vim 2> /dev/null || which vi)
